a0eb240fa5f4028a308ef912a587c5486b24cf71
easyadin <tobiayokunnu@gmail.com>
2020-09-06 06:34:29 +0400

    created selection samples

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -62,7 +62,115 @@ export class OnboardingPage implements OnInit, AfterViewInit {
     }
   }
 
-  onSkip(){
+  onSkip() {
     this.onNext();
+  }
+
+  // EXAMPLE SELECTIONS
+
+  singleSelect;
+  // single selection
+  onSingleSelect(selection) {
+    this.singleSelect = selection;
+  }
+
+  multiSelect: any[] = [];
+  options = [
+    {
+      id: 1,
+      name: "Tomato",
+      img: "../../assets/temp/tomato.jpg"
+    },
+    {
+      id: 2,
+      name: "Orange Juice",
+      img: "../../assets/temp/16571.png"
+    },
+    {
+      id: 3,
+      name: "Apple",
+      img: "../../assets/temp/6739.png"
+    }
+  ]
+
+  // multi selection
+  onMultiSelect(selection) {
+    if (this.multiSelect.includes(selection)) {
+      this.multiSelect = this.multiSelect.filter(s => s.id !== selection.id)
+    }
+    else {
+      this.multiSelect.push(selection)
+    }
+  }
+
+  multiSelect2: any[] = [];
+  options2 = [
+    {
+      id: 1,
+      name: "Tomoto",
+    },
+    {
+      id: 2,
+      name: "Apple",
+    },
+    {
+      id: 3,
+      name: "Orange",
+    },
+    {
+      id: 4,
+      name: "Painting",
+    },
+    {
+      id: 5,
+      name: "Sky Diving",
+    },
+    {
+      id: 6,
+      name: "Cheese",
+    },
+    {
+      id: 7,
+      name: "Eggplant",
+    },
+    {
+      id: 8,
+      name: "Chess",
+    }
+  ]
+
+  // multi selection
+  onMultiSelect2(selection) {
+    if (this.multiSelect2.includes(selection)) {
+      this.multiSelect2 = this.multiSelect2.filter(s => s.id !== selection.id)
+    }
+    else {
+      this.multiSelect2.push(selection)
+    }
+  }
+
+  multiSelect3: any[] = [];
+  options3 = [
+    {
+      id: 1,
+      name: "Designs",
+      img: '../../assets/temp/pen.svg'
+    },
+    {
+      id: 2,
+      name: "Projects",
+      img: '../../assets/temp/project.svg'
+    },
+
+  ]
+
+  // multi selection
+  onMultiSelect3(selection) {
+    if (this.multiSelect3.includes(selection)) {
+      this.multiSelect3 = this.multiSelect3.filter(s => s.id !== selection.id)
+    }
+    else {
+      this.multiSelect3.push(selection)
+    }
   }
 }
